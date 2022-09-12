7948b98ecca97856311b2d17a4c3786ec33f1481
xGuache <jalejovz@gmail.com>
2022-09-11 22:28:50 -0500

    Adicion de super usuario.

--- backend_django/home_care/homeCareApp/admin.py
+++ backend_django/home_care/homeCareApp/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Signos_vitales, Rol, Usuarios, Persona, Auxiliar, Enfermero, Familiar, Especialidad, Medico, Paciente, Registro_SV, T_Diagnostico, T_Sugerencias
+admin.site.register([Signos_vitales, Rol, Usuarios, Persona, Auxiliar, Enfermero, Familiar, Especialidad, Medico, Paciente, Registro_SV, T_Diagnostico, T_Sugerencias])
