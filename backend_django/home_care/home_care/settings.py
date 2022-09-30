376eb0d3b180501ccd4581a08c794c18ba929dda
unknown <jachilag@gmail.com>
2022-09-29 22:14:12 -0500

    correccion CORSE en BE

--- backend_django/home_care/home_care/settings.py
+++ backend_django/home_care/home_care/settings.py
@@ -25,8 +25,10 @@ SECRET_KEY = 'django-insecure-hceoli#+g@gy!aiyi0ox%_qtjjl-)vr4qkk#3rhyixs6jsg6wy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -37,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'homeCareApp',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -136,5 +140,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #NO BORRAR POR FAVOR
-#import django_heroku
-#django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
