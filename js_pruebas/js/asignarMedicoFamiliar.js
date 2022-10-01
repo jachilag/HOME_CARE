0183dd1781cd2cc1b6f616a9398c02d483aaa784
unknown <jachilag@gmail.com>
2022-10-01 01:46:15 -0500

    consultaPacientes y asignarMedicoFamiliar en js

---
+++ js_pruebas/js/asignarMedicoFamiliar.js
@@ -0,0 +1,275 @@
+
+// const host = 'http://127.0.0.1:8000/';
+const host = 'https://home-care-db-2022-g7.herokuapp.com/';
+
+const getUsuUrl = host + 'getPersonas';
+const updMedUrl = host + 'updatePaciente_Medico';
+const userId = sessionStorage.getItem('clientId');
+
+let usuarios = [];
+
+
+function sendData(data) {
+    accessToken = sessionStorage.getItem('accessToken');
+
+    fetch(host + userId, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + accessToken
+        },
+        body: data
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.text()
+            } else {
+                throw new Error(response.status)
+            }
+        })
+        .then(data => {
+            console.log(data);
+            alert('Datos actualizados');
+            goBack();
+        })
+        .catch(err => {
+            console.log("Error: " + err);
+            alert('Error al actualizar datos');
+            goBack();
+        });
+}
+
+function create() {
+    getUsuarios()
+    setTimeout(function(){
+        var identificacion = document.getElementById("Identificacion").value.trim();
+        var Password = document.getElementById("Password").value.trim();
+        var Nombre = document.getElementById("Nombre").value.trim();
+        var Apellido = document.getElementById("Apellido").value.trim();
+        var Telefono = document.getElementById("Telefono").value.trim();
+        var Genero = document.getElementById("Genero").value.trim();
+        var Email = document.getElementById("Email").value.trim();
+        
+        var Medico_ID_MEDICO = document.getElementById("Medico_ID_MEDICO").value.trim();
+        var Familiar_ID_FAMILIAR = document.getElementById("Familiar_ID_FAMILIAR").value.trim();
+        var Direccion = document.getElementById("Direccion").value;
+        var Ciudad = document.getElementById("Ciudad").value.trim();
+        var Latitud = document.getElementById("Latitud").value.trim();
+        var Longitud = document.getElementById("Longitud").value.trim();
+        var Fecha_Nacimiento = document.getElementById("Fecha_Nacimiento").value.trim();
+        
+        if(!comprobarInfoBasica(identificacion,Password,Nombre,Apellido,Telefono,Genero,Email)){
+            return;
+        }
+
+        if(!comprobarInfoEspecifica(Medico_ID_MEDICO, Familiar_ID_FAMILIAR, Direccion, Ciudad, Latitud, Longitud, Fecha_Nacimiento)){
+            return;
+        }
+            
+        var respuesta = confirm("Está seguro de crear el Paciente?")
+
+        if (respuesta) {
+            const data = {
+                "Identificacion": identificacion,
+                "Password": Password,
+                "Nombre": Nombre,
+                "Apellido": Apellido,
+                "Telefono": Telefono,
+                "Genero": Genero,
+                "Email": Email,
+
+                "Medico_ID_MEDICO" : Medico_ID_MEDICO==""?null:Medico_ID_MEDICO,
+                "Familiar_ID_FAMILIAR" : Familiar_ID_FAMILIAR==""?null:Familiar_ID_FAMILIAR,
+                "Direccion" : Direccion,
+                "Ciudad" : Ciudad,
+                "Latitud" : Latitud,
+                "Longitud" : Longitud,
+                "Fecha_Nacimiento" : Fecha_Nacimiento
+            };
+            
+            const dataToSend = JSON.stringify(data);
+            
+            newEntidad(dataToSend);
+        } else {
+            alert("Proceso cancelado.");
+        }
+    }, 600);
+}
+
+function comprobarInfoBasica(Identificacion,Password,Nombre,Apellido,Telefono,Genero,Email){
+    
+    if (Identificacion === ""){
+        alert("escriba su Identificacion")
+        return false;
+    }
+    if (getPersona(Number(Identificacion,"Paciente", false))){
+        alert("Ya existe Usuario")
+        return false;
+    }
+    if (Identificacion === ""){
+        alert("escriba su Identificacion")
+        return false;
+    }
+    if (Password === ""){
+        alert("escriba su Password")
+        return false;
+    }
+    if (Nombre === ""){
+        alert("escriba su Nombre")
+        return false;
+    }
+    if (Apellido === ""){
+        alert("escriba su Apellido")
+        return false;
+    }
+    if (Telefono === ""){
+        alert("escriba su Telefono")
+        return false;
+    }
+    if (Genero === "null"){
+        alert("Seleccione Genero")
+        return false;
+    }
+    if (Email === ""){
+        alert("escriba su Email")
+        return false;
+    }
+
+    return true
+}
+
+function comprobarInfoEspecifica(Medico_ID_MEDICO, Familiar_ID_FAMILIAR, Direccion, Ciudad, Latitud, Longitud, Fecha_Nacimiento){
+    if (Medico_ID_MEDICO != ""){
+        if (!getPersona(Number(Medico_ID_MEDICO,"Médico", true))){
+            alert("No existe medico con ese ID")
+            return false;
+        }
+    }
+    if (Familiar_ID_FAMILIAR != ""){
+        if (!getPersona(Number(Familiar_ID_FAMILIAR,"Familiar", true))){
+            alert("No existe familiar con ese ID")
+            return false;
+        }
+    }
+    if (Direccion === ""){
+        alert("escriba su Direccion")
+        return false;
+    }
+    if (Ciudad === ""){
+        alert("escriba su Ciudad")
+        return false;
+    }
+    if (Latitud === ""){
+        alert("escriba su Latitud")
+        return false;
+    }
+    if (Longitud === ""){
+        alert("escriba su Longitud")
+        return false;
+    }
+    if (Fecha_Nacimiento === ""){
+        alert("escriba su Fecha de nacimiento")
+        return false;
+    }
+    return true
+}
+
+function getUsuarios() {
+    fetch(getUsuUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.text()
+            } else {
+                console.log(response.body)
+                throw new Error(response.status)
+            }
+        })
+        .then(data => {
+            usuarios = JSON.parse(data);
+            console.log(usuarios);
+        })
+        .catch(err => {
+            console.log("Error: " + err);
+        });
+}
+
+function getPersona(Identificacion,rol,RolIgual){
+    let salida = false
+
+    usuarios.forEach((usu) => {
+        if(RolIgual){
+            if(usu.Identificacion==Identificacion && rol== usu.Rol ){
+                salida = true
+            }
+        } else{
+            if(usu.Identificacion==Identificacion){
+                salida = true
+            }
+        }
+    });
+
+    console.log(salida)
+    return salida
+}
+
+
+function RespExitosa(mess) {
+    alert("Paciente " + mess + " exitosamente!!");
+    location.reload();
+}
+
+function validar_medico(Medico_ID_MEDICO) {
+    if (Medico_ID_MEDICO != ""){
+        if (!getPersona(Number(Medico_ID_MEDICO,"Médico", true))){
+            return false;
+        }
+    }
+}
+
+function validar_familiar(Familiar_ID_FAMILIAR) {
+    if (Familiar_ID_FAMILIAR != ""){
+        if (!getPersona(Number(Familiar_ID_FAMILIAR,"Familiar", true))){
+            return false;
+        }
+    }
+}
+
+function collectData(evt) {
+    evt.preventDefault();
+    var id_medico = document.getElementById("Medico_ID_MEDICO").value.trim();
+    var id_familiar = document.getElementById("Familiar_ID_FAMILIAR").value.trim();
+        
+    let result = true
+    if (id_medico) {
+        result = validar_medico(id_medico);
+        if (!result) {
+            alert("No existe medico con ese ID");
+            return;
+        }
+    }
+    if (id_familiar) {
+        result = validar_familiar(id_familiar);
+        if (!result) {
+            alert("No existe familiar con ese ID");
+            return;
+        }
+    }
+
+    const customer = {}
+
+    if (firstName)
+        customer.firstName = firstName;
+    if (lastName)
+        customer.lastName = lastName;
+    if (email)
+        customer.email = email;
+    if (password)
+        customer.password = password;
+
+    console.log(customer);
+
+    const dataToSend = JSON.stringify(customer);
+    sendData(dataToSend);
+}
+document.actualizar.addEventListener('submit', collectData);
+document.addEventListener('DOMContentLoaded', showOldData);
---
+++ js_pruebas/js/consultaPacientes.js
@@ -0,0 +1,127 @@
+// const host = 'http://127.0.0.1:8000/';
+const host = 'https://home-care-db-2022-g7.herokuapp.com/';
+
+const getPacUrl = host + 'getPacientes';
+const getPacIndUrl = host + 'getPaciente';
+let Pac = [];
+PacInd = [];
+
+
+function getPacientes() {
+    fetch(getPacUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.text()
+            } else {
+                throw new Error(response.status)
+            }
+        })
+        .then(data => {
+            Pac = JSON.parse(data);
+            console.log(Pac);
+            pullPac();
+        })
+        .catch(err => {
+            console.log("Error: " + err);
+        });
+}
+
+function pullPac() {
+    const table = [];
+    console.log(Pac.length);
+    if (Pac.length > 0) {
+        // document.getElementById("notFound").remove();
+
+        Pac.forEach((pac) => {
+            const tr = document.createElement("tr");
+            // tr.innerHTML = `<tr><td>${pac.Identificacion}</td><td>${pac.Medico_ID_MEDICO}</td><td><i class="bi bi-pencil" onclick='showModal(${esp.id}, "${esp.Especialidad}")'></i></td><td><i class="bi bi-trash3" onclick='showModalEli(${esp.id}, "${esp.Especialidad}")'></i></td></tr>`;
+            tr.innerHTML = `<tr><td>${pac.Identificacion}</td><td>${pac.Nombre}</td><td>${pac.Apellido}</td><td>${pac.Telefono}</td><td>${pac.Direccion}</td></tr>`;
+            table.push(tr);
+        });
+        const info = document.getElementById("tableall");
+        table.forEach(tr => info.appendChild(tr));
+    }
+}
+
+
+
+var one = document.getElementById('one');
+one.style.display = 'none';
+
+var all = document.getElementById('all');
+all.style.display = 'none';
+
+function viewOne() {
+    var all = document.getElementById('all');
+    all.style.display = 'none';
+
+    var one = document.getElementById('one');
+    one.style.display = 'block';
+
+    var search = document.getElementById('search');
+    search.style.display = 'block';
+
+    var result = document.getElementById('result');
+    result.style.display = 'none';
+
+
+}
+
+function viewAll() {
+    var one = document.getElementById('one');
+    one.style.display = 'none';
+
+    var all = document.getElementById('all');
+    all.style.display = 'block';
+
+}
+
+function validate() {
+    var id = document.getElementById('identificacion').value;
+    console.log(id);
+
+    if (id == '' || id == undefined) {
+        alert('Debe diligenciar un valor válido para la identificación del paciente!!!');
+    } else {
+        var search = document.getElementById('search');
+        search.style.display = 'none';
+
+        var result = document.getElementById('result');
+        result.style.display = 'block';
+
+        fetch(getPacIndUrl + '/' + id)
+            .then(response => {
+                if (response.ok) {
+                    return response.text()
+                } else {
+                    throw new Error(response.status)
+                }
+            })
+            .then(data => {
+                PacInd = JSON.parse(data);
+                console.log(PacInd);
+                fillPac();
+            })
+            .catch(err => {
+                console.log("Error: " + err);
+            });
+
+    }
+}
+
+function fillPac() {
+    document.getElementById('ide').innerHTML = PacInd.Identificacion;
+    document.getElementById('Medico_ID_MEDICO').innerHTML = PacInd.Medico_ID_MEDICO;
+    document.getElementById('Familiar_ID_FAMILIAR').innerHTML = PacInd.Familiar_ID_FAMILIAR;
+    document.getElementById('Nombre').innerHTML = PacInd.Nombre;
+    document.getElementById('Apellido').innerHTML = PacInd.Apellido;
+    document.getElementById('Genero').innerHTML = PacInd.Genero;
+    document.getElementById('Telefono').innerHTML = PacInd.Telefono;
+    document.getElementById('Fecha_Nacimiento').innerHTML = PacInd.Fecha_Nacimiento;
+    document.getElementById('Direccion').innerHTML = PacInd.Direccion;
+    document.getElementById('Ciudad').innerHTML = PacInd.Ciudad;
+    document.getElementById('Latitud').innerHTML = PacInd.Latitud;
+    document.getElementById('Longitud').innerHTML = PacInd.Longitud;
+}
+
+document.addEventListener("DOMContentLoaded", getPacientes);
